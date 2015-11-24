42a80a0f9f4a38d12f439ad4cd9d292960413a22
Willem Meints <willem@mein-design.nl>
2015-11-24 21:30:10 +0100

    Upgrade to new API

--- helloworld/consumer/src/main/java/nl/infosupport/kafkaworkshop/helloworld/ConsumerApp.java
+++ helloworld/consumer/src/main/java/nl/infosupport/kafkaworkshop/helloworld/ConsumerApp.java
@@ -1,17 +1,11 @@
 package nl.infosupport.kafkaworkshop.helloworld;
 
-import kafka.consumer.*;
-import kafka.message.MessageAndMetadata;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.*;
 
 public class ConsumerApp {
     private static final Logger log = LoggerFactory.getLogger(ConsumerApp.class);
@@ -23,72 +17,25 @@ public class ConsumerApp {
         //  - group ID to identify the application that is fetching (you can have multiple instances of a consumer)
 
         Properties consumerProperties = new Properties();
-        consumerProperties.put("zookeeper.connect", "localhost:2181");
+        consumerProperties.put("bootstrap.servers", "localhost:9092");
         consumerProperties.put("group.id", "consumerapp");
+
+        // Set some default deserializers for the consumed messages.
+        // This is required because otherwise Kafka doesn't know what to do with the data.
+        consumerProperties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProperties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         // Use autocommit settings so that you don't need to commit the messages you received.
         // This is typically done in scenarios where consistency isn't an issue.
+        consumerProperties.put("enable.auto.commit", "true");
         consumerProperties.put("auto.commit.interval.ms", "1000");
 
-        kafka.javaapi.consumer.ConsumerConnector consumerConnector =
-                Consumer.createJavaConsumerConnector(
-                        new ConsumerConfig(consumerProperties));
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(consumerProperties);
+        consumer.subscribe(Arrays.asList("events"));
 
-        // Kafka is capable of using multiple threads to process incoming messages.
-        // Please keep in mind that using more threads doesn't mean that your application will be faster.
-        // This setting requires tuning, most likely you're going to use ma. 2 * CPU_COUNT as the setting for this.
-        // However, if you require more power in the rest of the app you are best off testing things out using a
-        // performance tool.
-        int threadCount = 2;
-
-        Map<String,Integer> topicCount = new HashMap<>();
-        topicCount.put("events",threadCount);
-
-        Map<String,List<KafkaStream<byte[],byte[]>>> consumerStreams =
-                consumerConnector.createMessageStreams(topicCount);
-
-        ExecutorService threadPool = Executors.newFixedThreadPool(threadCount);
-
-        for(KafkaStream<byte[],byte[]> stream: consumerStreams.get("events")) {
-            log.info("Starting message handler for topic events");
-            threadPool.submit(new MessageHandler(stream));
-        }
-
-        // The rest of this method is a nasty trick to prevent Java from shutting down on us.
-        // Please ignore this and don't use if you have better ways to control the runtime.
-        Semaphore signal = new Semaphore(0,false);
-
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                threadPool.shutdown();
-                signal.release();
-            }
-        });
-
-        signal.acquire();
-    }
-
-    private static class MessageHandler implements Runnable {
-        private final KafkaStream<byte[],byte[]> stream;
-        private final Logger log = LoggerFactory.getLogger(MessageHandler.class);
-
-        public MessageHandler(KafkaStream<byte[], byte[]> stream) {
-            this.stream = stream;
-        }
-
-        public void run() {
-            // The iterator used in this method never ends unless you stop the threadpool
-            // This means that you don't have to worry about exit conditions here ;-)
-            ConsumerIterator<byte[], byte[]> iterator = stream.iterator();
-
-            while(iterator.hasNext()) {
-                MessageAndMetadata<byte[], byte[]> messageData = iterator.next();
-
-                // WARNING: If your producer does not provide a key, this method will never complete.
-                // This is due to the fact that Kafka blocks the call until it finds the data for the key, which
-                // is of course not available if you don't provide a key.
-                log.info("Received message [{}] {}", messageData.key(),messageData.message());
-            }
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            records.forEach(msg -> log.info("Received message: {}", msg.value()));
         }
     }
 }
--- helloworld/producer/src/main/java/nl/infosupport/kafkaworkshop/helloworld/ProducerApp.java
+++ helloworld/producer/src/main/java/nl/infosupport/kafkaworkshop/helloworld/ProducerApp.java
@@ -1,8 +1,7 @@
 package nl.infosupport.kafkaworkshop.helloworld;
 
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
-import kafka.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
@@ -15,19 +14,22 @@ public class ProducerApp {
         //  - A serializer so that messages can be converted to Kafka specific bits
         // - The number of acknowledgements you want back before the message is considered to be sent succesfully
 
-        producerProperties.put("metadata.broker.list","localhost:9092");
-        producerProperties.put("serializer.class","kafka.serializer.StringEncoder");
-        producerProperties.put("request.required.acks","1");
+        producerProperties.put("bootstrap.servers", "localhost:9092");
+        producerProperties.put("serializer.class", "kafka.serializer.StringEncoder");
+        producerProperties.put("request.required.acks", "1");
 
-        ProducerConfig producerConfig = new ProducerConfig(producerProperties);
-        Producer<String,String> producer = new Producer<String,String>(producerConfig);
+        // Set some default deserializers for the consumed messages.
+        // This is required because otherwise Kafka doesn't know what to do with the data.
+        producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(producerProperties);
 
         // Send a single keyed message to the server.
         // Each message in Kafka has a key which you can set to anything you like.
         // It also has a payload, which you can fill with any data you like as long as it can be
         // serialized by the encoder. Normally you'd use something like BSON or JSON here.
-        KeyedMessage<String,String> msg = new KeyedMessage<String, String>(
-                "events","HelloWorldEvent","Hello world!");
+        ProducerRecord<String, String> msg = new ProducerRecord<String, String>("events", "Hello world!");
 
         producer.send(msg);
     }
