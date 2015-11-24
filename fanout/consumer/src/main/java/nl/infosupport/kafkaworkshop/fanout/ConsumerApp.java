4af11153122d930d79da513dcc5a67997ad4dfa8
Willem Meints <willem@mein-design.nl>
2015-11-24 21:51:37 +0100

    Fix fanout demo

--- fanout/consumer/src/main/java/nl/infosupport/kafkaworkshop/fanout/ConsumerApp.java
+++ fanout/consumer/src/main/java/nl/infosupport/kafkaworkshop/fanout/ConsumerApp.java
@@ -1,9 +1,13 @@
 package nl.infosupport.kafkaworkshop.fanout;
 
 import org.apache.commons.cli.*;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Properties;
 import java.util.concurrent.Semaphore;
 
 public class ConsumerApp {
@@ -12,7 +16,7 @@ public class ConsumerApp {
     public static void main(String[] args) throws Exception {
 
         Options opts = new Options();
-        opts.addOption(Option.builder("zookeeper").desc("Zookeeper host to connect to").hasArg().required().build());
+        opts.addOption(Option.builder("broker").desc("Broker host to connect to").hasArg().required().build());
         opts.addOption(Option.builder("topic").desc("Topic to consume from").hasArg().required().build());
         opts.addOption(Option.builder("group").desc("Group identifier for the consumer").hasArg().required().build());
 
@@ -22,28 +26,36 @@ public class ConsumerApp {
 
             CommandLine cmd = parser.parse(opts, args);
 
-            kafka.javaapi.consumer.ConsumerConnector consumer = ConsumerConnectorFactory.createConsumer(
-                    cmd.getOptionValue("zookeeper"), cmd.getOptionValue("group"));
+            Properties consumerProperties = new Properties();
+            consumerProperties.put("bootstrap.servers", cmd.getOptionValue("broker"));
+            consumerProperties.put("group.id", cmd.getOptionValue("group"));
 
-            MessageListener listener = new MessageListener(consumer);
+            // Set some default deserializers for the consumed messages.
+            // This is required because otherwise Kafka doesn't know what to do with the data.
+            consumerProperties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+            consumerProperties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-            // Start to listen on the specified topic.
-            // You can start the listener just once in this demo.
-            listener.start(cmd.getOptionValue("topic"), 1, msg -> {
-                log.info("Received message: {}", msg);
-            });
+            // Use autocommit settings so that you don't need to commit the messages you received.
+            // This is typically done in scenarios where consistency isn't an issue.
+            consumerProperties.put("enable.auto.commit", "true");
+            consumerProperties.put("auto.commit.interval.ms", "1000");
 
-            // The rest of this method is a nasty trick to prevent Java from shutting down on us.
-            // Please ignore this and don't use if you have better ways to control the runtime.
-            Semaphore signal = new Semaphore(0, false);
+            KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(consumerProperties);
 
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    signal.release();
-                }
-            });
+            // Always call subscribe first!
+            // Otherwise you can poll all you want, but get nothing in return.
+            consumer.subscribe(Arrays.asList(cmd.getOptionValue("topic")));
 
-            signal.acquire();
+            while (true) {
+                // Use a polling timeout to retrieve messages.
+                // Increase the timeout value to slow down the message flow and decrease the load on the brokers.
+                // You will get more messages each time you poll with a longer timeout setting.
+                // But it does mean you have to wait longer for the messages to come in.
+                ConsumerRecords<String, String> records = consumer.poll(100);
+
+                // Process the messages here.
+                records.forEach(msg -> log.info("Received message: {}", msg.value()));
+            }
         }
         catch(ParseException ex) {
             System.out.println(ex.getMessage());
--- fanout/consumer/src/main/java/nl/infosupport/kafkaworkshop/fanout/MessageAction.java
+++
@@ -1,7 +0,0 @@
-package nl.infosupport.kafkaworkshop.fanout;
-
-
-@FunctionalInterface
-public interface MessageAction {
-    void execute(String messageContent);
-}
